723aaf8fa35f5f6bb0f9289bb8d4f83fd71cc555
andy <yxchen1994@gmail.com>
2018-04-24 14:36:08 +0800

    update
    second initialization

--- app/contracts/writing_share.sol
+++ app/contracts/writing_share.sol
@@ -1,117 +1,188 @@
 pragma solidity ^0.4.17;
 
+
+
 contract WritingShare {
 
-	uint stage = 0; //0:have not begin ,1:begin, 2:vote begin, 3:vote finish
-	uint beginMoney = 200;
-	uint currentChapter = 0;
-	uint charpterCount = 3;
-	uint beginChapterTime;
-	uint writingTime = 200;
-	uint voteTime = 200;
-	uint candidateCost = 10 ether;
-	uint votingCost = 10 ether;
-	address drawerAddress;
+    
+    //variables about stages and chapters
+	uint stage = 0; //0:have not begin   1:begin candidate  2:vote begin   3:vote finish and distribute money
+	uint currentChapter = 0;   //index of currentChapter, begin with 0
+	uint constant chapterAmount = 3;   // chapterAmount   
+	
+    // variables about time 
+	uint writingTime = 200;    // the time for candidates to write and broadcast
+	uint votingTime = 200;       // the time for voting
+	//varaibles about cost
+	uint beginCost = 200; //the amount Drawer initially sends to the contract
+	uint writingCost = 10 ether;  //  the price for each candidate
+	uint votingCost = 10 ether;    //  the price for each voting tiket in voting stage
+	//varaiable about reward
+	uint rewardPercentOfCandidate = 40; // represent as percentages
+	uint rewardPercentOfDrawer = 40;
+	
+	address drawerAddress;   // the address of drawer
 
+    
 	modifier onlyDrawer() {
 	    require(msg.sender == drawerAddress);
 	    _;
   	}
-
-
+    modifier onlyStage0(){
+        require(stage==0);
+        _;
+    }    
+    modifier onlyStage1(){
+        require(stage==1);
+        _;
+    }
+    modifier onlyStage2(){
+        require(stage==2);
+        _;
+    }
+    modifier onlyStage3(){
+        require(stage==3);
+        _;
+    }
+    
 	struct chapter{
-		uint numCandidate;
+		uint numCandidates;
 		uint beginChapterTime;
 		uint writingEnd;
-		uint voteEnd;
+		uint votingEnd;
 		uint[] votes;
+		uint totalMoney;
 		mapping (uint=>address) addressCandidates;
 		mapping (uint=>string) contentCandidates;
 	}
 
 	chapter[] private chapters;
 
-	function WritingShare() public{
-
+	function WritingShare(address _drawerAddress) public{
+        drawerAddress = _drawerAddress;
 	}
-
-	function createNewChapter() onlyDrawer payable public {
-
-
-		require(stage==0);
+    
+    
+	function createNewChapter() payable public onlyDrawer onlyStage0 {
+	    require(currentChapter<=chapterAmount);
+        //pay money
 		uint extra = msg.value;
 		if(currentChapter==0){
-			require(msg.value>=beginMoney);
-			extra = msg.value - beginMoney;
+			require(msg.value>=beginCost);
+			extra = msg.value - beginCost;
 		}
 		if(extra>0){msg.sender.transfer(extra);}
 
-
+        //create new chapter
 		chapters.push(chapter({
-
-			numCandidate: 0,
+			numCandidates: 0,
 			votes: new uint[](0),
 			beginChapterTime: now,
+			totalMoney:0,
 			writingEnd: now+writingTime,
-			voteEnd:now+writingTime+voteTime
+			votingEnd:now + writingTime+votingTime
 	    }));
+
 	    stage = 1; 
-
 	}
 
-	function createNewCandidate(string _content) payable public{
-		require(msg.value>=candidateCost);
-		require(now<=chapters[currentChapter].writingEnd);
+	function createNewCandidate(string _content) payable public onlyStage1 {
+	    //pay money
+		require(msg.value>=writingCost);
+		uint extra = msg.value - writingCost;
+		if(extra>0){msg.sender.transfer(extra);}
 
-		uint extra = msg.value - candidateCost;
-		if(extra>0){
-			msg.sender.transfer(extra);
-		}
-
+        //add candidates
 		chapter storage ThisChapter = chapters[currentChapter];
 
-		ThisChapter.addressCandidates[ThisChapter.numCandidate] = msg.sender;
-		ThisChapter.contentCandidates[ThisChapter.numCandidate] = _content;
-		ThisChapter.votes[ThisChapter.numCandidate] = 0;
-		ThisChapter.numCandidate ++;
+		ThisChapter.addressCandidates[ThisChapter.numCandidates] = msg.sender;
+		ThisChapter.contentCandidates[ThisChapter.numCandidates] = _content;
+		ThisChapter.votes.push(0);
+		ThisChapter.numCandidates ++;
+		ThisChapter.totalMoney += writingCost;
+
+	}
+	function beginVoting() public onlyDrawer onlyStage1{
+	    require(now>=chapters[currentChapter].writingEnd);
+	    stage = 2;
 	}
 
-	function voting(uint _indexVoteCandidate) payable public{
-		require(stage==2);
-		require(now<=chapters[currentChapter].voteEnd);
+	function voting(uint _indexVoteCandidate) payable public onlyStage2{
+		
+        // make sure that the index is valid
+		chapter storage ThisChapter = chapters[currentChapter];
+		require(_indexVoteCandidate>=0 && _indexVoteCandidate<ThisChapter.numCandidates);
+		
+		//pay money
 		require(msg.value>=votingCost);
-
-		chapter storage ThisChapter = chapters[currentChapter];
-
-		require(_indexVoteCandidate>=0 && _indexVoteCandidate<ThisChapter.numCandidate);
-
 		uint extra = msg.value;
-
-		extra = msg.value - votingCost;
-		msg.sender.transfer(extra);
-
-		ThisChapter.votes[_indexVoteCandidate] ++;
+		while(extra>=votingCost){
+		    extra = extra - votingCost;
+		    ThisChapter.votes[_indexVoteCandidate] ++;  //take a vote
+		    ThisChapter.totalMoney +=votingCost;  
+		}
+        if(extra>0){msg.sender.transfer(extra);}
 
 	}
-
-	function endCandidate() onlyDrawer internal {
-		chapter storage ThisChapter = chapters[currentChapter];		
-		require(now>=ThisChapter.writingEnd);
-		stage = 2;
-		ThisChapter.voteEnd = now + voteTime;
+    
+	function endVoting() public onlyDrawer onlyStage2 {
+	    //change stage
+		chapter storage ThisChapter = chapters[currentChapter];	
+		require(now>=ThisChapter.votingEnd);
+		stage = 3;
+		
+		reward();
+		
+		stage = 0;
+	    currentChapter +=1;
 	}
 
+    function reward() internal onlyDrawer onlyStage3{
+        chapter storage ThisChapter = chapters[currentChapter];	
+        
+        //make sure there is at least one candidate for this chapter
+        require(ThisChapter.numCandidates>0);
+        
+        //find the index of winner
+        uint indexWinner = 0;
+        for(uint i=1;i<ThisChapter.numCandidates;i++)
+        {
+            if(ThisChapter.votes[i]>ThisChapter.votes[indexWinner]){
+                indexWinner = i;
+            }
+        }
+        
+        address winnerAddress = ThisChapter.addressCandidates[indexWinner];
+        uint256 moneyForWinner = div(uint256(ThisChapter.totalMoney*rewardPercentOfCandidate),uint256(100));
+        winnerAddress.transfer(moneyForWinner);
+        
+        uint256 moneyForDrawer = div(uint256(ThisChapter.totalMoney*rewardPercentOfDrawer),uint256(100));
+        drawerAddress.transfer(moneyForDrawer);
+    }
+      /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
 
-	function checkCurrentChapter() public view returns (uint) {
+    
+	function getCurrentChapter() public view returns (uint) {
 		return currentChapter;
 	}	
 
-	function checkStage() public view returns (uint) {
+	function getStage() public view returns (uint) {
 		return stage;
 	}
 
-	function checkBalance()  public view returns(uint){
+	function getBalance()  public view returns(uint){
 		return this.balance;
+  	}
+  	function () payable public{
+
   	}
 
 
